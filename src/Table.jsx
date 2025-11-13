1448466def784e172a533a0d71635f4e219c3d39
erfan-ne <erfan.neshati@gmail.com>
2025-11-13 12:37:46 +0330

    update Table component

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,5 +1,18 @@
 import React, { useEffect, useState } from "react";
 
 export default function Table() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(()=> {
+        fetch("https://jsonplaceholder.typicode.com/todos")
+        .then((Response) => Response.json())
+        .then(todosData => setTodos(todosData))
+  }, [])
+
   
+  return (
+    <>
+      {todos.map(todo => console.log(todo))}
+    </>
+  );
 }
